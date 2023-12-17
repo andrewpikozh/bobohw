436f9d1c1018b43c51bbf928f010f909bc011cd1
Andrew Pikozh <pikozh13@gmail.com>
2023-12-17 22:31:56 +0200

    initial

---
+++ nasademo/src/main/java/com/example/nasademo/NasademoApplication.java
@@ -0,0 +1,13 @@
+package com.example.nasademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasademoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NasademoApplication.class, args);
+    }
+
+}
---
+++ nasademo/src/test/java/com/example/nasademo/NasademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nasademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasademoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
